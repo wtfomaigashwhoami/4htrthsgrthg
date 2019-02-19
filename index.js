d56276634141f0afe2e8a6df8924a28acb46c2d0
wtfomaigashwhoami <47795996+wtfomaigashwhoami@users.noreply.github.com>
2019-02-19 23:28:26 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+const Discord = require('discord.js');
+const prefix = '***';
+var client = new Discord.Client();
+
+
+client.on('ready', () => {
+    console.log(client.user.username + ' is on');
+	client.user.setActivity('redd.atshop.io', {type: "PLAYING"});
+});
+
+client.on('message', message => {
+
+    if(message.content.startsWith(prefix + 'dm')) {
+
+        let messageArray = message.content.split(" ");
+        let args = messageArray.slice(1);
+        if(message.channel.type === "dm") {
+            return;
+        }
+        
+        if(!message.member.hasPermission("ADMINISTRATOR")) {
+			return message.channel.send('No perms :*');
+		}
+        
+        if(!args.join(" ")) {
+            return message.channel.send('Include a message to send');
+        }
+
+        message.guild.members.forEach(member => {
+            if (member.id !== client.user.id) {
+                try {
+                    member.send(args.join(" "));
+                } catch (e) {
+                    console.log('Someone cannot be dmed sorry.')
+                }
+            }
+        });
+
+        message.channel.send('Message `' + args.join(" ") + '` was successfully sent to all members in this discord.').catch(() => console.log('Someone have his DM locked or he blocked me L'));
+    }
+});
+
+client.login(process.env.BOT_TOKEN);
